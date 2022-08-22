449e7801b2de114395c541cda0b35a39c2abcce7
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-08-22 12:37:01 +0300

    добавила task 7, 8, 9, 10

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,27 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    console.dir(formElements);
+
+    const mail = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        mail,
+        password,
+    };
+
+    console.log(formData); 
+
+    if (mail === "" || password === "") {
+        alert("Всі поля повинні бути заповнені");
+    }
+
+    event.currentTarget.reset();
+}
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,48 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector('#controls > input');
+const btnCreate = document.querySelector('button[data-create]');
+const btnDestroy = document.querySelector('button[data-Destroy');
+const boxesContainer = document.querySelector('#boxes');
+
+input.addEventListener('change', onInputChange);
+
+function onInputChange(event) {
+  const numberInput = event.currentTarget.value;
+  console.log(numberInput);
+}
+
+btnCreate.addEventListener('click', onBtnCreateClick);
+
+function onBtnCreateClick(event) {
+  console.log('Create');
+  createBoxes('numberInput');
+}
+
+function createBoxes(amount) {
+  const boxes = [];
+  
+  for (let i = 1; i <= amount; i += 1){
+    const box = document.createElement('div');
+    box.style.width = '30px';
+    box.style.height = '30px';
+    box.backgroundColor = getRandomHexColor();
+
+    boxes.push(box);
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+btnDestroy.addEventListener('click', onBtnDestroyClick);
+
+function onBtnDestroyClick(event) {
+  console.log('Destroy');
+  destroyBoxes();
+}
+
+function destroyBoxes() {
+  boxesContainer.remove();
+}
