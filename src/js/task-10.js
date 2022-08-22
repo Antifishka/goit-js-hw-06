165ef577b56941bb7c303e9d4dcd760a9792027c
Antifishka <101981622+Antifishka@users.noreply.github.com>
2022-08-22 14:46:41 +0300

    редактировала задачу 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -7,40 +7,24 @@ const btnCreate = document.querySelector('button[data-create]');
 const btnDestroy = document.querySelector('button[data-Destroy');
 const boxesContainer = document.querySelector('#boxes');
 
-input.addEventListener('change', onInputChange);
-
-function onInputChange(event) {
-  const numberInput = event.currentTarget.value;
-  console.log(numberInput);
-}
-
-btnCreate.addEventListener('click', onBtnCreateClick);
-
-function onBtnCreateClick(event) {
-  console.log('Create');
-  createBoxes('numberInput');
-}
+btnCreate.addEventListener('click', createBoxes);
+btnDestroy.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
   const boxes = [];
+  amount = input.value;
   
   for (let i = 1; i <= amount; i += 1){
     const box = document.createElement('div');
     box.style.width = '30px';
     box.style.height = '30px';
-    box.backgroundColor = getRandomHexColor();
+    box.style.backgroundColor = getRandomHexColor();
 
     boxes.push(box);
   }
 
+  console.log(boxes);
   boxesContainer.append(...boxes);
-}
-
-btnDestroy.addEventListener('click', onBtnDestroyClick);
-
-function onBtnDestroyClick(event) {
-  console.log('Destroy');
-  destroyBoxes();
 }
 
 function destroyBoxes() {
